7a056fc57d7896e42c19d96fe478b9a3a48a49cd
Johnnie Emmanuel <udomajohn21@gmail.com>
2023-09-27 21:13:26 -0700

    collecting matches for the day from various leagues

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 import os
+import time
 
 # Specify the path to the Edge WebDriver executable
 driver_path = r'C:\Users\hp\Downloads\edgedriver_win64\msedgedriver.exe'
@@ -12,62 +13,108 @@ os.environ['PATH'] += os.pathsep + driver_path
 driver = webdriver.Edge()
 
 # URL of the page you want to scrape
-url = "https://us.soccerway.com/"
-driver.get(url)
+main_url = "https://us.soccerway.com/"
+driver.get(main_url)
 
 # Wait for the page to load (you may need to adjust the waiting time)
 driver.implicitly_wait(10)
 
-# Extract the page source
-page_source = driver.page_source
-
-# Parse the HTML content of the page with BeautifulSoup
-soup = BeautifulSoup(page_source, "html.parser")
-
-# Find all matchinfo elements
-matchinfo_elements = soup.find_all("div", class_="matchinfo")
-
 # Initialize a list to store match details
 matches = []
 
-for matchinfo_element in matchinfo_elements:
-    # Extract match details from each matchinfo element
-    kickoff_time = matchinfo_element.find("time", class_="kickoff_time").text.strip()
-    
-    teams = matchinfo_element.find_all("div", class_="team_name")
-    team1 = teams[0].text.strip()
-    team2 = teams[1].text.strip()
-    
-    match_status = matchinfo_element.find("div", class_="match_status").text.strip()
-    
-    # Initialize scores for both teams as None
-    score_team1 = None
-    score_team2 = None
-    
-    # Check if the match has started
-    if match_status != "-":
-        # Extract the scores for both teams
-        score_elements = matchinfo_element.find_all("span", class_="team_score")
-        if len(score_elements) == 2:
-            score_team1 = score_elements[0].text.strip()
-            score_team2 = score_elements[1].text.strip()
-    
-    # Create a dictionary to represent the match
-    match_info = {
-        "Kickoff Time": kickoff_time,
-        "Team 1": team1,
-        "Team 2": team2,
-        "Match Status": match_status,
-        "Score Team 1": score_team1,  # Include the score for Team A
-        "Score Team 2": score_team2,  # Include the score for Team 
-    }
-    
-    # Append the match info to the list
-    matches.append(match_info)
+# Counter for the number of leagues processed
+league_count = 0
+
+try:
+    while True:
+        try:
+            # Find all league elements
+            league_elements = driver.find_elements(By.CLASS_NAME, "livescores-comp")
+
+            for league_element in league_elements:
+                if league_count >= 15:
+                    break  # Exit the loop once 15 leagues have been processed
+
+                # Check if the league is expanded by checking the "data-expanded" attribute
+                is_expanded = league_element.get_attribute("data-expanded") == "1"
+                
+                if not is_expanded:
+                    # If the league is not expanded, click the "expand-icon" button
+                    try:
+                        # Scroll into view to ensure the button is clickable
+                        driver.execute_script("arguments[0].scrollIntoView();", league_element)
+                        expand_icon_button = league_element.find_element(By.CLASS_NAME, "expand-icon")
+                        expand_icon_button.click()
+                        # Wait for a moment to load the matches
+                        time.sleep(2)
+                    except Exception as e:
+                        print(f"Error clicking 'expand-icon' button: {e}")
+                        # Handle the error, e.g., log it or skip this league
+                        continue
+
+                # Extract league details
+                league_name = league_element.find_element(By.CLASS_NAME, "area-name").text.strip()
+
+                # Find all matchinfo elements within the league element
+                matchinfo_elements = league_element.find_elements(By.CLASS_NAME, "livescore_match")
+
+                for matchinfo_element in matchinfo_elements:
+                    # Extract match details from each matchinfo element
+                    kickoff_time = matchinfo_element.find_element(By.CLASS_NAME, "kickoff_time").text.strip()
+
+                    teams = matchinfo_element.find_elements(By.CLASS_NAME, "team_name")
+                    team1 = teams[0].text.strip()
+                    team2 = teams[1].text.strip()
+
+                    match_status = matchinfo_element.find_element(By.CLASS_NAME, "match_status").text.strip()
+
+                    # Initialize scores for both teams as None
+                    score_team1 = None
+                    score_team2 = None
+
+                    # Check if the match has started
+                    if match_status != "-":
+                        # Extract the scores for both teams
+                        score_elements = matchinfo_element.find_elements(By.CLASS_NAME, "team_score")
+                        if len(score_elements) == 2:
+                            score_team1 = score_elements[0].text.strip()
+                            score_team2 = score_elements[1].text.strip()
+
+                    # Create a dictionary to represent the match
+                    match_info = {
+                        "League Name": league_name,
+                        "Kickoff Time": kickoff_time,
+                        "Team 1": team1,
+                        "Team 2": team2,
+                        "Match Status": match_status,
+                        "Score Team 1": score_team1,  # Include the score for Team A
+                        "Score Team 2": score_team2,  # Include the score for Team B
+                    }
+
+                    # Append the match info to the list
+                    matches.append(match_info)
+
+                # Increment the league count
+                league_count += 1
+
+            if league_count >= 15:
+                break  # Exit the loop once 15 leagues have been processed
+
+            # Find the next page button if available and click it
+            next_page_button = driver.find_element(By.XPATH, "//a[contains(text(), 'Next')]")
+            if next_page_button:
+                next_page_button.click()
+            else:
+                break  # No more pages
+
+        except Exception as e:
+            print(f"Error scraping: {e}")
+            break  # Break the loop if an error occurs
+
+finally:
+    # Close the WebDriver session when done
+    driver.quit()
 
 # Print the list of matches
 for match in matches:
     print(match)
-
-# Close the WebDriver session when done
-driver.quit()
